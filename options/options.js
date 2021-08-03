ab27f21ea66d96dd825f6a78b5bd09077c34a46f
Crowdbotics <team@crowdbotics.com>
2021-08-03 11:08:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "focus_2_29385Identifier",
+  url: "https://focus_2_29385Identifier.botics.co",
+  api: "https://focus_2_29385Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
